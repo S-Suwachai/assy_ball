653d92dff33b93cb3e5935cac004ed0dda3291ee
S-Suwachai <suwachai.salatip@gmail.com>
2024-01-24 17:08:21 +0700

    change host to server in backend file

--- backend/instance/ms_instance.js
+++ backend/instance/ms_instance.js
@@ -1,6 +1,5 @@
 const Sequelize = require("sequelize");
 const dbms = new Sequelize("machine_data", "sa", "sa@admin", {
-    // host: "192.168.100.10",
     server: "192.168.100.10",
     port : "1433",
     dialect: "mssql",
