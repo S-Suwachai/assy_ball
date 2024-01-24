68f372a9c2105c44444ac2fb29ba1e9326f07bf4
S-Suwachai <suwachai.salatip@gmail.com>
2024-01-24 17:39:07 +0700

    delete port 1433

--- backend/instance/ms_instance.js
+++ backend/instance/ms_instance.js
@@ -1,7 +1,7 @@
 const Sequelize = require("sequelize");
 const dbms = new Sequelize("machine_data", "sa", "sa@admin", {
     server: "192.168.100.10",
-    port : "1433",
+    // port : "1433",
     dialect: "mssql",
     dialectOptions: {
         options: {
