095ebd844e0cf0ded0279899f2b024c53e9056c3
S-Suwachai <suwachai.salatip@gmail.com>
2024-01-24 17:33:05 +0700

    edit for clone to server

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
 app.use("/assy/ball", require("./api/api_assyBall"));
-app.use("/assy/alarmlist", require("./api/api_assyAlarmlist"));
 
 app.listen(4000, () => {
     console.log("Server is running on port 4000");
