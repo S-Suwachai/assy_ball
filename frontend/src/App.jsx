fb94e1e7c6a1b68b24ecc28692f6a84f93d8b094
S-Suwachai <suwachai.salatip@gmail.com>
2024-01-26 10:11:54 +0700

    test commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Sidemenu from "./components/Sidemenu";
+// import Header from "./components/Header";
+// import Footer from "./components/Footer";
+// import Sidemenu from "./components/Sidemenu";
 
 import UsageBallDaily from "./pages/assembly/UsageBallDaily";
 
