dbd4087bb271b8e31c0ab9fa9cd0c7913bd57672
S-Suwachai <suwachai.salatip@gmail.com>
2024-01-26 08:26:50 +0700

    edit formula instance/ms_instance.js

--- backend/instance/ms_instance.js
+++ backend/instance/ms_instance.js
@@ -1,14 +1,29 @@
 const Sequelize = require("sequelize");
-const dbms = new Sequelize("machine_data", "sa", "sa@admin", {
-    server: "192.168.100.10",
-    port : "1433",
-    dialect: "mssql",
+
+// const dbms = new Sequelize("machine_data", "sa", "sa@admin", {
+//     server: "192.168.100.10",
+//     port : "1433",
+//     dialect: "mssql",
+//     dialectOptions: {
+//         options: {
+//             instanceName: "SQLEXPRESS",
+//         },
+//     },
+// });
+
+const dbms = new Sequelize({
+    dialect: 'mssql',
+    host: "192.168.100.10",
+    username: "sa",
+    password: "sa@admin",
+    database: "machine_data",
     dialectOptions: {
         options: {
             instanceName: "SQLEXPRESS",
-        },
-    },
+        }
+    }
 });
+
 (async () => {
     await dbms.authenticate();
 })();
