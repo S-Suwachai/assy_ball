34b07a7442f111a1df441d94180c3b027e3e3285
S-Suwachai <suwachai.salatip@gmail.com>
2024-01-26 10:01:39 +0700

    comment for test

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,21 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Header from "./components/Header.jsx";
-import Footer from "./components/Footer.jsx";
-import Sidemenu from "./components/Sidemenu.jsx";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Sidemenu from "./components/Sidemenu";
 
-import UsageBallDaily from "./pages/assembly/UsageBallDaily.jsx";
+import UsageBallDaily from "./pages/assembly/UsageBallDaily";
 
 export default function App() {
     return (
         <BrowserRouter>
             <div>
-                <Header />
-                <Sidemenu />
+                {/* <Header /> */}
+                {/* <Sidemenu /> */}
                 <Routes>
                     <Route path="/UsageBallDaily" element={<UsageBallDaily />} />
                     <Route exact={true} path="*" element={<UsageBallDaily />} />
                 </Routes>
-                <Footer />
+                {/* <Footer /> */}
             </div>
         </BrowserRouter>
     );
